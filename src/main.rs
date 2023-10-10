0dff285664849b34a292e7c3be44e5c13a94fe06
Hou Neavireakpong <neavireakpong.hou@gmail.com>
2023-10-10 21:27:37 +0700

    Create endpoint and migrations

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use axum::http;
-use axum::routing::{get, Router};
+mod handlers;
+use std::env;
+
+use axum::routing::{get, Router, post};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+    let port = env::var("PORT").unwrap_or("3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
+    // let database_url = env::var("DATABASE_URL").expect("missing DATABASE_URL env");
+    let database_url = "postgres://postgres:123@localhost:5432/quotes?sslm";
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
     let app = Router::new()
-        .route("/health", get(health));
+        .route("/health", get(handlers::health))
+        .route("/quote", post(handlers::create_quote))
+        .with_state(pool);
 
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
@@ -15,8 +28,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
     Ok(())
-}
-
-async fn health() -> http::StatusCode {
-    http::StatusCode::OK
 }
